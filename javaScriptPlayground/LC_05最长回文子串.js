3ff4a79e43c3a54391e0f190d5df43acdf9ed8f5
殷魄 <dvyinpo@163.com>
2020-07-23 23:13:25 +0800

    最大回文子串，动态规划解法

---
+++ javaScriptPlayground/LC_05最长回文子串.js
@@ -0,0 +1,32 @@
+/**
+ * @param {string} s
+ * @return {string}
+ * dynamic planning
+ * 'baab'
+ * "babad"
+ * 'bcaaacbaabd'
+ * 'abbbb'
+ */
+var longestPalindrome = function (s) {
+  const dp = [];
+  let maxS = '';
+  for (let i = s.length - 1; i >= 0; i--) {
+    dp[i] = [];
+    for (let j = i; j < s.length; j++) {
+      if (i === j) {
+        dp[i][j] = true;
+      } else if (j - i === 1 && s[i] === s[j]) {
+        dp[i][j] = true;
+      } else if (j - i > 1 && dp[i + 1][j - 1] && s[i] === s[j]) {
+        dp[i][j] = true;
+      } else {
+        dp[i][j] = false;
+      }
+      if (dp[i][j] && j - i + 1 > maxS.length) {
+        maxS = s.substring(i, j + 1);
+      }
+    }
+  }
+  return maxS;
+};
+console.log(longestPalindrome('cbbd'));
